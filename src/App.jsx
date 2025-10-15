99d69f216b350e0b32b17f7d5135e90effdbd104
Almaz0825 <almazasaviev052@gmail.com>
2025-10-15 13:07:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <img src="src/assets/account.jpg" alt="1" />
       <h1>Ясавиев Алмаз Ильнарович 426-ВЕБ</h1>
     </>
   )
